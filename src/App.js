df150d67e3c2eaf592fed5c35a62fa12da2c43b3
monicatemburu <saimonicasowmyasri@gmail.com>
2021-12-18 16:13:14 +0530

    Initial c

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Calculator from './components/calculator';
+import Test from './components/test';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Calculator/>
+        {/* <Calculator/> */}
+        <Test/>
       </header>
     </div>
   );
---
+++ src/components/test.jsx
@@ -0,0 +1,32 @@
+import '../styles/test.css';
+
+function test(){
+    return(
+        <div className="Calci">
+            <input />
+            <div className="Calci-Buttons">
+                
+                    <button className="is-clear">C</button>
+                    <button className="Calci-Btn">+</button>
+                    <button className="Calci-Btn">9</button>
+                    <button className="Calci-Btn">8</button>
+                    <button className="Calci-Btn">7</button>
+                    <button className="Calci-Btn">-</button>
+                    <button className="Calci-Btn">6</button>
+                    <button className="Calci-Btn">5</button>
+                    <button className="Calci-Btn">4</button>
+                    <button className="Calci-Btn">*</button>
+                    <button className="Calci-Btn">3</button>
+                    <button className="Calci-Btn">2</button>
+                    <button className="Calci-Btn">1</button>
+                    <button className="Calci-Btn">/</button>
+                    <button className="is-zero">0</button>
+                    <button className="Calci-Btn">=</button>
+                    
+                
+            </div>
+        </div>
+    )
+}
+
+export default test;
